d12b26fc7e66424d36d418bd61e881b1630e33d9
Susan Kamies <susan@Susans-MacBook-Pro.local>
2018-01-12 21:01:04 +0100

    restore to default exercise

--- TouchTracker/TouchTracker/DrawView.swift
+++ TouchTracker/TouchTracker/DrawView.swift
@@ -10,13 +10,8 @@ import UIKit
 
 class DrawView: UIView {
     
-    //MARK: variables
-    
     var currentLines = [NSValue:Line]()
     var finishedLines = [Line]()
-    
-    var currentCircle = [NSValue:Circle]()
-    var finishedCircles = [Circle]()
     
     @IBInspectable var finishedLineColor: UIColor = UIColor.black{
         didSet{
@@ -46,8 +41,6 @@ class DrawView: UIView {
         path.stroke()
     }
     
-    
-    
     override func draw(_ rect: CGRect) {
         //Teken lijnen die af zijn in het zwart
         finishedLineColor.setStroke()
@@ -58,114 +51,28 @@ class DrawView: UIView {
         currentLineColor.setStroke()
         for (_, line) in currentLines {
             stroke(line)
-            let angle = angleBetweenPoints(startingPoint: line.begin, endPoint: line.end)
-            if angle >= 90 && angle < 180 {
-                currentLineColor = UIColor.green
-            }
-            else if angle >= 180 && angle < 270 {
-                currentLineColor = UIColor.blue
-            }
-            else if angle >= 270 && angle < 360 {
-                currentLineColor = UIColor.yellow
-            }
-            else {
-                currentLineColor = UIColor.red
-            }
-        }
-        
-        finishedLineColor.setStroke()
-        for circle in finishedCircles {
-            let path = UIBezierPath(ovalIn: circle.rectangle)
-            path.lineWidth = lineThickness
-            path.lineCapStyle = .round
-            
-            path.stroke()
-
-        }
-        
-        currentLineColor.setStroke()
-        for (_, circle) in currentCircle {
-            let path = UIBezierPath(ovalIn: circle.rectangle)
-            path.lineWidth = lineThickness
-            path.lineCapStyle = .round
-            
-            path.stroke()
-            
         }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         //Log statement om de volgorde van events te kunnen bekijken
         print(#function)
-        if touches.count == 2 {
-            let firstTouch = touches.first!
-            let lastTouch = touches.reversed().first!
+        
+        for touch in touches {
+            let location = touch.location(in: self)
             
-            let firstLocation = firstTouch.location(in: self)
-            let lastLocation  = lastTouch.location(in: self)
+            let newLine = Line(begin: location, end: location)
             
-            let xDist = (firstLocation.x + lastLocation.x) / 2
-            let yDist = (firstLocation.y + lastLocation.y) / 2
-            
-            let diameter = distanceBetweenTwoPoints(point1: firstLocation, point2: lastLocation)
-            
-            
-            let rectangle = CGRect(x: xDist - (diameter/2), y: yDist - (diameter/2) , width: diameter , height: diameter)
-            
-            let key = NSValue(nonretainedObject: firstTouch)
-            currentCircle[key] = Circle(rectangle: rectangle)
-            
-            
+            let key = NSValue(nonretainedObject: touch)
+            currentLines[key] = newLine
         }
-        else {
-            for touch in touches {
-                let location = touch.location(in: self)
-                
-                let newLine = Line(begin: location, end: location)
-                
-                let key = NSValue(nonretainedObject: touch)
-                currentLines[key] = newLine
-                
-                
-            }
-        }
+        
         setNeedsDisplay()
     }
-    
-    func distanceBetweenTwoPoints(point1: CGPoint, point2: CGPoint) -> CGFloat {
-        let xDist = abs(point1.x - point2.x)
-        let yDist = abs(point1.y - point2.y)
-        
-        return CGFloat(sqrt(xDist * xDist) + sqrt(yDist * yDist))
-    }
-    
-    
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         print(#function)
         
-        if touches.count == 2 {
-            let firstTouch = touches.first!
-            let lastTouch = touches.reversed().first!
-            
-            let firstLocation = firstTouch.location(in: self)
-            let lastLocation  = lastTouch.location(in: self)
-            
-            let xDist = (firstLocation.x + lastLocation.x) / 2
-            let yDist = (firstLocation.y + lastLocation.y) / 2
-            
-            print("\(xDist)")
-            print("\(yDist)")
-            
-            let diameter = distanceBetweenTwoPoints(point1: firstLocation, point2: lastLocation)
-            
-            
-            let rectangle = CGRect(x: xDist - (diameter/2), y: yDist - (diameter/2) , width: diameter , height: diameter)
-            
-            let key = NSValue(nonretainedObject: firstTouch)
-            currentCircle[key]? = Circle(rectangle: rectangle)
-            
-        }
         for touch in touches {
             let key = NSValue(nonretainedObject: touch)
             currentLines[key]?.end = touch.location(in: self)
@@ -176,16 +83,6 @@ class DrawView: UIView {
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         print(#function)
-        if touches.count == 2 {
-            let firstTouch = touches.first!
-            
-            let key = NSValue(nonretainedObject: firstTouch)
-            if let circle = currentCircle[key] {
-                finishedCircles.append(circle)
-                currentCircle.removeValue(forKey: key)
-            }
-            
-        }
         
         for touch in touches {
             let key = NSValue(nonretainedObject: touch)
@@ -206,21 +103,5 @@ class DrawView: UIView {
         currentLines.removeAll()
         
         setNeedsDisplay()
-    }
-    
-    func originPoint(start: CGPoint, end: CGPoint) -> CGPoint {
-        return CGPoint(x: end.x - start.x, y: end.y - start.y)
-    }
-    
-    
-    func angleBetweenPoints(startingPoint: CGPoint, endPoint: CGPoint) -> Double {
-        
-        let origin = originPoint(start: startingPoint, end: endPoint)
-        let bearingRadius = atan2f(Float(origin.y), Float(origin.x))
-        var  bearingDegrees = bearingRadius * Float((180.0 / Double.pi))
-        
-        bearingDegrees = bearingDegrees > 0.0 ? bearingDegrees : (360.0 + bearingDegrees)
-        
-        return Double(bearingDegrees)
     }
 }
